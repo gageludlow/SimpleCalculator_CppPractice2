497e740a54fd3a27e68eba955edd00bc0181de8c
Gage Ludlow <gageludlow@gmail.com>
2025-08-01 14:59:31 -0700

    finish simple calc using std::function in a std::map. map the operators like + or - to the std::function that returns the result of the operation. can add more like making it loop until user exits. and add more complex operations like 5 + 2 * 3 while following order of operations.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <functional>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main(){
 
-	cout << "Hello World" << endl;
+	map<char, function<double(double, double)>> operations;
 
+	operations['-'] = [](double x, double y) { return x - y; };
+	operations['+'] = [](double x, double y) { return x + y; };
+	operations['*'] = [](double x, double y) { return x * y; };
+	operations['/'] = [](double x, double y) { return y != 0 ? x / y : 0;};
+
+	
+	double a = 0.0;
+	double b = 0.0;
+	char op;
+
+	cout << "Input desired calculation. eg. '5 + 3'" << endl;
+	cin >> a >> op >> b;
+
+	if (operations.find(op) != operations.end())
+	{
+		cout << "Result: ";
+		cout << operations[op](a, b) << endl;
+	}
+	else
+	{
+		cout << "Error: Unknown Input" << endl;
+	}
 
 	return 0;
 
